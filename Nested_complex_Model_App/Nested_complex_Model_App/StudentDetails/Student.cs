dad9e7b61d2e3912615e2fb1c61f091b3890f9e6
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-01 17:51:19 +0530

    sample(GH-3805):Nested_ComplexModel_App

---
+++ Nested_complex_Model_App/Nested_complex_Model_App/StudentDetails/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace NestedComplexModel
+{
+    public class Student
+    {
+        public Student ()
+        {
+            Details = new PersonalDetails();
+        }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [ValidateComplexType]
+        public PersonalDetails Details { get; set; }
+    }
+
+    public class PersonalDetails
+    {
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
