67ae65c8ec4fcabfa39914d594a2b43562d89626
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-01 18:51:39 +0530

    sample(GH-3805):Editcontext use in Form

---
+++ Edit_context_app/Edit_context_app/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Edit_context_app
+{
+    public class Customer
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(4, ErrorMessage = "The organization code must be less than 5   characters in length.")]
+        public string OrganizationCode { get; set; }
+    }
+}
