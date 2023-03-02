aa7c196eee4708c359dfdad037909072298d131a
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-02 19:09:47 +0530

    Add files via upload

---
+++ Enable_Validation_without_Using_DataAnnotationValidator/Blazor_Server_App/FluentValidation/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor_Server_App.FluentValidation
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Organization { get; set; }
+    }
+}
