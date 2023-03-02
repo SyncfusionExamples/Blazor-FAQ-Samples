e618fcdf70fe00bb3fe66dc2e19bf66a32cc8fa0
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-02 20:03:01 +0530

    Add files via upload

---
+++ Create_Custom_Validation_Component/BlazorServerApp/Pages/CustomValidatorAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BlazorServerApp.Pages
+{
+    public class CustomValidationAttribute : ValidationAttribute
+    {
+        public string ValidPassword { get; set; }
+
+        protected override ValidationResult IsValid ( object password, ValidationContext validationContext )
+        {
+            var content = password.ToString().ToLower();
+            if (content.Equals(ValidPassword.ToLower()))
+            {
+                return null;
+            }
+            return new ValidationResult(ErrorMessage, new[] { validationContext.MemberName });
+        }
+    }
+}
