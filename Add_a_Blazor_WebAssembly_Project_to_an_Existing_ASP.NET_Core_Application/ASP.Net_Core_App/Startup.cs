0d644f8dc7e712cf7c2e0ca254995ff4fb5452ae
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-03 16:36:23 +0530

    Add files via upload

---
+++ Add_a_Blazor_WebAssembly_Project_to_an_Existing_ASP.NET_Core_Application/ASP.Net_Core_App/Startup.cs
@@ -0,0 +1,16 @@
+﻿public class Startup
+{
+    public void Configure ( IApplicationBuilder app, IWebHostEnvironment env )
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+
+        app.UseBlazorFrameworkFiles();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
