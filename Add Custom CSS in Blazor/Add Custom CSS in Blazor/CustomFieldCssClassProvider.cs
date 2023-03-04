18df756d050c8584dd94f766ed7fe0a625b9f904
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-04 14:16:56 +0530

    Add files via upload

---
+++ Add Custom CSS in Blazor/Add Custom CSS in Blazor/CustomFieldCssClassProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Add_Custom_CSS_in_Blazor
+{
+    public class CustomFieldCssClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass ( EditContext editContext,
+        in FieldIdentifier fieldIdentifier )
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return isValid ? "validField" : "invalidField";
+        }
+    }
+}
---
+++ Add Custom CSS in Blazor/Add Custom CSS in Blazor/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Add_Custom_CSS_in_Blazor
+{
+    public class Customer
+    {
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        [StringLength(4, ErrorMessage = "The organization code must be less than 5   characters in length.")]
+        public string? OrganizationCode { get; set; }
+    }
+}
