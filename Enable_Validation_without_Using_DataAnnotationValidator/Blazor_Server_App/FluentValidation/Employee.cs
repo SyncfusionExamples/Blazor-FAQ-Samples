e0a9773088dbb20fc4ebfea9326dfda303732ff3
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-04 15:37:12 +0530

    Delete Enable_Validation_without_Using_DataAnnotationValidator directory

--- Enable_Validation_without_Using_DataAnnotationValidator/Blazor_Server_App/FluentValidation/Employee.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Blazor_Server_App.FluentValidation
-{
-    public class Employee
-    {
-        public string Name { get; set; }
-        public string Organization { get; set; }
-    }
-}
