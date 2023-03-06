e55bab20538c3133ff6e28e1a6bad73b12047818
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-06 09:57:15 +0530

    Add files via upload

---
+++ Add a Blazor WebAssembly Project to an Existing ASP.NET Core Application/ASP .Net Core App/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Services;
+public class Startup
+{
+    public void Configure ( IApplicationBuilder app, IWebHostEnvironment env )
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+
+        app.UseBlazorFrameworkFiles();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapFallbackToFile("index.html");
+        });
+    }
+}
