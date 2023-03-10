fbdf969b77c17d1fd0f8998fc5205b2f6f727295
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-10 10:35:20 +0530

    Add files via upload

---
+++ Using Blazor in Existing ASP.NET MVC App/ASP.Net core App MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.Net_core_App_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
