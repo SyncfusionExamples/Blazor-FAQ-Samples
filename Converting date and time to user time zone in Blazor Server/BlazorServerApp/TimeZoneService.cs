7c0cdf41cd121cd17e433b5dade71e809a79ceb5
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-21 18:01:14 +0530

    Add files via upload

---
+++ Converting date and time to user time zone in Blazor Server/BlazorServerApp/TimeZoneService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.JSInterop;
+namespace BlazorServerApp
+{
+    public class TimeZoneService
+    {
+         private readonly IJSRuntime _jsRuntime;
+
+        private TimeSpan? _userOffset;
+
+        public TimeZoneService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async ValueTask<DateTimeOffset> GetLocalDateTime(DateTimeOffset dateTime)
+        {
+            if (_userOffset == null)
+            {
+                int offsetInMinutes = await _jsRuntime.InvokeAsync<int>("blazorGetTimezoneOffset");
+                _userOffset = TimeSpan.FromMinutes(-offsetInMinutes);
+            }
+
+            return dateTime.ToOffset(_userOffset.Value);
+        }
+    }
+    }
+
