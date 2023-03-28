9beb8e146998fc7bd9616a78cccaf11fe29598de
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-28 15:21:25 +0530

    Add files via upload

---
+++ Using Google Maps in Blazor/BlazorServerApp/wwwroot/script.js
@@ -0,0 +1,9 @@
+﻿
+function initialize() {
+    var latlng = new google.maps.LatLng(40.716948, -74.003563);
+    var options = {
+        zoom: 14, center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), options);
+}
