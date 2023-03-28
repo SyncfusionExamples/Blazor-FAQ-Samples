c58b981501f5c2ddb03505db0b8643ab4dbc9588
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-03-28 15:04:57 +0530

    Add files via upload

---
+++ Using jQuery UI components in Blazor/Blazor Server App/wwwroot/script.js
@@ -0,0 +1,10 @@
+﻿var clickCount = 0;
+
+function renderjQueryComponents() {
+    $("#accordion").accordion();
+    $(".jquery-btn button").button();
+    $(".jquery-btn button").click(function () {
+        console.log('Clicked');
+        $('.click-count')[0].innerText = ++clickCount;
+    });
+}
