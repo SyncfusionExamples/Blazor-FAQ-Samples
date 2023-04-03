d3651bc17c1e0a04cb583837abc0efcb98e367a1
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-03 11:12:09 +0530

    Add files via upload

---
+++ Using IndexedDB in Blazor WASM App/Blazor WebAssembly App/Data/ExampleDb.cs
@@ -0,0 +1,26 @@
+﻿using IndexedDB.Blazor;
+using Microsoft.JSInterop;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_WebAssembly_App.Data
+{
+    public class ExampleDb : IndexedDb
+    {
+        public ExampleDb ( IJSRuntime jSRuntime, string name, int version ) : base(jSRuntime, name, version) { }
+
+        // These are like tables. Declare as many of them as you want.
+        public IndexedSet<Person> People { get; set; }
+    }
+
+    public class Person
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+    }
+}
