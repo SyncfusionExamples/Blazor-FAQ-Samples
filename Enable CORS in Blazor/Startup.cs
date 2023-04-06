76e1a45f4f8657c931ad875fffb9550c7f44cbf4
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-06 19:33:14 +0530

    Add files via upload

---
+++ Enable CORS in Blazor/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace BlazorServerApp
+{
+    public class Startup
+    {
+        public void ConfigureServices ( IServiceCollection services )
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("NewPolicy", builder =>
+                 builder.AllowAnyOrigin()
+                              .AllowAnyMethod()
+                              .AllowAnyHeader());
+            });
+        }
+        public void Configure ( IApplicationBuilder app, IWebHostEnvironment env )
+        {
+            app.UseRouting();
+            app.UseCors("NewPolicy");
+            app.UseAuthorization();
+
+        }
+
+    }
+}
