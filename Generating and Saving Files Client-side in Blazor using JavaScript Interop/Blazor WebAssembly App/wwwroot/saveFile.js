ec552dd8abb7d3d6a41af149439b59786fa1c5c7
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-06 19:56:47 +0530

    Delete Generating and Saving Files Client-side in Blazor using JavaScript Interop directory

--- Generating and Saving Files Client-side in Blazor using JavaScript Interop/Blazor WebAssembly App/wwwroot/saveFile.js
+++
@@ -1,8 +0,0 @@
-﻿function saveFile(file, Content) {
-    var link = document.createElement('a');
-    link.download = name;
-    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(Content)
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
