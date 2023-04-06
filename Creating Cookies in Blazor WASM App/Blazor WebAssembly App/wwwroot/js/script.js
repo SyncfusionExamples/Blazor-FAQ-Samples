71bc3e2f834fdb924156194076d8a0fd44784201
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-06 11:02:50 +0530

    Add files via upload

---
+++ Creating Cookies in Blazor WASM App/Blazor WebAssembly App/wwwroot/js/script.js
@@ -0,0 +1,14 @@
+﻿window.methods = {
+    CreateCookie: function (name, value, days) {
+        var expires;
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
+        }
+        else {
+            expires = "";
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+}
