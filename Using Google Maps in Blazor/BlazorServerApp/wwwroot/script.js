017c3be45e2f04429704ec3a349dab1035b4d573
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-12 15:43:51 +0530

    Delete Using Google Maps in Blazor directory

--- Using Google Maps in Blazor/BlazorServerApp/wwwroot/script.js
+++
@@ -1,9 +0,0 @@
-﻿
-function initialize() {
-    var latlng = new google.maps.LatLng(40.716948, -74.003563);
-    var options = {
-        zoom: 14, center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map"), options);
-}
