5f67a3d18d48a14936632d1726713575bd4efdb5
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-12 17:32:18 +0530

    Delete Validation without DataAnnotation in Blazor directory

--- Validation without DataAnnotation in Blazor/Validation without DataAnnotation/FluentValidation/Employee.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Validation_without_DataAnnotation.FluentValidation
-{
-    public class Employee
-    {
-        public string? Name { get; set; }
-        public string? Organization { get; set; }
-    }
-}
