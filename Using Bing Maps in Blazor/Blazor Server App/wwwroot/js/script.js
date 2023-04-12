36f30e6769650d7b3267b92a563f5ea3f9350ef0
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-12 15:26:49 +0530

    Delete Using Bing Maps in Blazor directory

--- Using Bing Maps in Blazor/Blazor Server App/wwwroot/js/script.js
+++
@@ -1,6 +0,0 @@
-﻿function loadBingMap() {
-    var map = new Microsoft.Maps.Map(document.getElementById('map'), {});
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
-    map.entities.push(pushpin);
-    return "";
-}
