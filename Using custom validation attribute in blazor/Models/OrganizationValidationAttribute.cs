1cdfb859f1a216d18fa48cce1a2ddb3e9491b412
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-12 16:40:44 +0530

    Add files via upload

---
+++ Using custom validation attribute in blazor/Models/OrganizationValidationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerApp.Models
+{
+    public class OrganizationValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid ( object? value, ValidationContext? validationContext )
+        {
+            if (value==null)
+            {
+                var errorMessage = FormatErrorMessage(validationContext!.DisplayName);
+                return new ValidationResult(errorMessage,new string[] {validationContext!.MemberName});
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
