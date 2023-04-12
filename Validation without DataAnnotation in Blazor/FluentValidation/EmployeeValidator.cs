17dac87e79910a1fd0b1ef7172b3d62be345f82d
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-12 17:33:07 +0530

    Add files via upload

---
+++ Validation without DataAnnotation in Blazor/FluentValidation/EmployeeValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Validation_without_DataAnnotation.FluentValidation
+{
+    public class EmployeeValidator : AbstractValidator<Employee>
+    {
+        public EmployeeValidator ()
+        {
+            RuleFor(p => p.Name).NotEmpty().WithMessage("You   must enter a valid name.");
+            RuleFor(p => p.Name).MaximumLength(20).WithMessage("The   name cannot be more than 20 characters long.");
+            RuleFor(p => p.Organization).NotEmpty().WithMessage("You   must enter a valid organization name.");
+        }
+    }
+}
