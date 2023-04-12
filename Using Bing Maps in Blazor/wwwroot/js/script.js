ae6293936a2a485d5823f219acf8d9aff1949459
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-12 15:27:41 +0530

    Add files via upload

---
+++ Using Bing Maps in Blazor/wwwroot/js/script.js
@@ -0,0 +1,6 @@
+﻿function loadBingMap() {
+    var map = new Microsoft.Maps.Map(document.getElementById('map'), {});
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
+    map.entities.push(pushpin);
+    return "";
+}
