d6748bde8740baaa764291bd6a5c54acbd0b9610
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-12 16:45:23 +0530

    Add files via upload

---
+++ Using different CSS files in Blazor/wwwroot/js/script.js
@@ -0,0 +1,7 @@
+﻿function includeCss(url) {
+    var element = document.createElement("link");
+    element.setAttribute("rel", "stylesheet");
+    element.setAttribute("type", "text/css");
+    element.setAttribute("href", url);
+    document.getElementsByTagName("head")[0].appendChild(element);
+}
