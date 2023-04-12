1aa206ecb27894f480a21d48c61c1197a6eef516
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-12 16:44:17 +0530

    Delete Using different CSS files in Blazor directory

--- Using different CSS files in Blazor/Blazor Server App/wwwroot/js/script.js
+++
@@ -1,7 +0,0 @@
-﻿function includeCss(url) {
-    var element = document.createElement("link");
-    element.setAttribute("rel", "stylesheet");
-    element.setAttribute("type", "text/css");
-    element.setAttribute("href", url);
-    document.getElementsByTagName("head")[0].appendChild(element);
-}
