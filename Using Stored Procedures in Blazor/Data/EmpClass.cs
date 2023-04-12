a416cdebf79b02b6d695a8a37f6b12f6da0888d8
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-12 16:06:48 +0530

    Add files via upload

---
+++ Using Stored Procedures in Blazor/Data/EmpClass.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerApp.Data
+{
+    public class EmpClass
+    {
+        [Key]
+        public int Empid { get; set; }
+        public string? Empname { get; set; }
+        public string? Department { get; set; }
+        public DateTime Jointdate { get; set;}
+
+    }
+}
