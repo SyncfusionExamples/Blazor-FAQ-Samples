ae747e2d3b122c6ac006e06247305f0e26644560
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-04-14 12:57:50 +0530

    Add files via upload

---
+++ Dynamic Component Creation with RenderTreeBuilder in Blazor/DynamicRenderComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorServerApp
+{
+    public class DynamicRenderComponent: ComponentBase
+    {
+        protected override void BuildRenderTree ( RenderTreeBuilder builder )
+        {
+
+            builder.OpenElement(0, "h1");
+            builder.AddContent(0, "This is Example of Dynamically Render Component");
+            builder.CloseElement();
+
+        }
+    }
+}
