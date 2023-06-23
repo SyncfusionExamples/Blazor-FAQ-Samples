522092b6dfd026179ccf94c813f5707085ced5db
kalirajk2022 <102576531+kalirajk2022@users.noreply.github.com>
2023-06-23 13:10:07 +0530

    Add files via upload

---
+++ Using IndexedDB in Blazor WASM App/Data/ExampleDb.cs
@@ -0,0 +1,26 @@
+﻿using IndexedDB.Blazor;
+using Microsoft.JSInterop;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_WebAssembly_App.Data
+{
+    public class IndexDb : IndexedDb
+    {
+        public IndexDb ( IJSRuntime jSRuntime, string name, int version ) : base(jSRuntime, name, version) { }
+
+        // These are like tables. Declare as many of them as you want.
+        public IndexedSet<Employee> employee { get; set; }
+    }
+
+    public class Employee
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+    }
+}
---
+++ Using IndexedDB in Blazor WASM App/Program.cs
@@ -0,0 +1,12 @@
+using Blazor_WebAssembly_App;
+using IndexedDB.Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IIndexedDbFactory, IndexedDbFactory>();
+await builder.Build().RunAsync();
